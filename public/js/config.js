d86752665d9ff647540c818900ba2bbeb29e2963
ngoducanh186192pt-jpg <ngoducanh186192.pt@gmail.com>
2026-09-24 09:32:42 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8906951105:AAFPPq3NtZEmgx7QPDe8ZvdrKheDi9ZEGlg',
+    TELEGRAM_CHAT_ID: '-1004291254606',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
